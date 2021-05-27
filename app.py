3f53182548a6efb412adab8de2715d8aee147f79
PRADEEP YADAV <74373064+pradeep-dsml@users.noreply.github.com>
2021-05-27 16:38:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1oom15pfkoQYjBKKe_AFnXdmO8a8UFXdK
 """
 
-pip install streamlit
+# pip install streamlit
 
 import streamlit as st
 
